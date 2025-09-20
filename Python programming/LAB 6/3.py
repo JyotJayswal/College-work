18c097b7d7f7a7683207e0c72788e61c30e2ff91
JyotJayswal <jyotjayswal268@gmail.com>
2025-09-20 17:02:00 +0530

    incomplete program-3  of lab 6 added in python

---
+++ Python programming/LAB 6/3.py
@@ -0,0 +1,8 @@
+# Suppose a date is represented as a tuple (d, m, y).Create two date tuples &
+# find the number of days between the two dates.
+
+print("Enter the dates in the (d,m,y) format.")
+date_1=tuple(input(f"Date_1:"))
+date_2=tuple(input(f"Date 2:"))
+
+print(date_1)
